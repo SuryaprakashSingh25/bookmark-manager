d3a7de6d358fe542c4eb647eca92d9bd4bd8bce6
SuryaprakashSingh25 <singh.suraj1025@gmail.com>
2026-05-23 19:03:51 +0530

    ROuter Outlet fix

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,19 +1,11 @@
-import { Component, signal, ViewChild } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { BookmarkForm } from './components/bookmark-form/bookmark-form';
-import { BookmarkList } from './components/bookmark-list/bookmark-list';
 
 @Component({
   selector: 'app-root',
-  imports: [BookmarkForm,BookmarkList],
+  standalone: true,
+  imports: [RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App {
-  @ViewChild(BookmarkList)
-  bookmarkList!:BookmarkList;
-
-  refreshBookmarks(){
-    this.bookmarkList.loadBookmarks();
-  }
-}
+export class App {}
